fbb12a07c29fd209266fea7128d4788ef24d91f8
Paulette Hellen Obimbo <paulettehellen8@gmail.com>
2026-06-17 15:44:36 +0300

    learnt primitive data types in JS

--- JS/Intro/index.js
+++ JS/Intro/index.js
@@ -3,3 +3,8 @@ let y = 34;
 let z = x + y;
             
 console.log(z);
+
+//myage
+myage =34;
+
+
---
+++ JS/Intro/primitiveDataType.js
@@ -0,0 +1,62 @@
+/*
+Numbers.
+floating point- decimal place
+real numbers
+positive numbers
+negative numbers
+*/
+
+let number1 =23; //real numbers
+console.log(number1);
+let number2 =34.34; //floating point
+console.log(number2);
+let number3 =-34; //negative numbers
+console.log(number3);
+
+//biggest number 1.7976931348623157 × 10^308
+
+/*
+strings store a sequence of characters
+1. ""
+2. ''
+3. `` <enables you to have multi line strings and include both "" and ''>
+*/
+
+let str1= "Double quote Paulette";
+console.log(str1);
+let str2= 'Single quote I am Paulette';
+console.log(str2);
+let str3= `Back Ticks I am awesome`;
+console.log(str3);
+let str4="23";
+console.log(str4);
+
+//Using multiple strings
+let str5='Daniel said "23 is a string"';
+console.log(str5);
+
+let str6= '23 is\'nt a number';
+console.log(str6);
+
+
+/*Boolean represent true or false */
+let johnIsMale=true;
+console.log(johnIsMale);
+let johnIsMarried=false;
+console.log(johnIsMarried);
+
+
+/*Null is lack or absence of a value */
+let nullVal1=null;
+console.log(nullVal1);
+
+
+/*Undefined is also lack of a value 
+NEVER set sth to undefined use null instead
+*/
+let undefinedVal1=undefined;
+console.log(undefinedVal1);
+let undefinedVal2;
+console.log(undefinedVal2);
+
+
