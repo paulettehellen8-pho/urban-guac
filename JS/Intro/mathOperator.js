70c90ddbc486ecf81b0027b68d30cd2d26e0a0b8
Paulette Hellen Obimbo <paulettehellen8@gmail.com>
2026-06-18 15:54:18 +0300

    learnt mathematical operators

---
+++ JS/Intro/mathOperator.js
@@ -0,0 +1,42 @@
+/*
+Assignment =
+*/
+
+let numb1=23;//Declared and assigned
+console.log(`numb1 is ${numb1} and its type is ${typeof numb1}`)
+numb1=40;//Assignment only
+console.log(`numb1 is ${numb1} and its type is ${typeof numb1}`)
+
+//Addition +
+let numb2=60;
+console.log(`numb2 is ${numb2} and its type is ${typeof numb2}`)
+let numb3=numb1+numb2+10;
+console.log(`numb3 is ${numb3} and its type is ${typeof numb3}`)
+
+//Multiplication and powers
+let numb4=numb1*numb2
+console.log(`numb4 is ${numb4} and its type is ${typeof numb4}`)
+let numb5=numb1**3//double aesthetics means to the power of
+console.log(`numb5 is ${numb5} and its type is ${typeof numb5}`)
+let numb6=Math.pow(10,3)//2^3
+console.log(`numb6 is ${numb6} and its type is ${typeof numb6}`)
+
+//Division /
+let numb7=30
+let numb8=10
+let divNumb=numb7/numb8
+console.log(`divNumb is ${divNumb} and its type is ${typeof divNumb}`)
+
+/*
+    Modulus gives the remainder (%)
+    Helps you to know if a number is a multiple or can be divided by a certain number
+*/
+let numb9=23
+let numb10=12
+let modNu=numb9%numb10
+console.log(`modNu is ${modNu} and its type is ${typeof modNu}`)
+
+
+
+
+
