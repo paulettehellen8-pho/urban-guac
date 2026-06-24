bf0136f4ba37f3ed4f204325be12bc558735bc54
Paulette Hellen Obimbo <paulettehellen8@gmail.com>
2026-06-24 11:29:55 +0300

    learnt if statement

--- JS/Intro/ifElseifElse.js
+++ JS/Intro/ifElseifElse.js
@@ -3,33 +3,40 @@
     If is independent -> One statement does not depend on another.
 */
 
-let age =prompt("Enter your age");
+let age =Number(prompt("Enter your age"));
+
+// console.log(`age is ${age} typeof ${typeof age}`);
 alert(`You enterd ${age}`)
 
-if (age>10){
-    alert('You are a baby');
-}
+// if (isNaN(age)){//You can also use Number or +age
 
-if (age>16){
-    alert('You are a teen');
-}
+    if (age>10){
+        alert('You are a baby');
+    }
 
-if (age>18){
-    alert("You can legally drink and smoke");
-    // console.log("But should you?")
-}
+    if (age>16){
+        alert('You are a teen');
+    }
 
-if (age>27){
-    alert("Best age for marriage")
-    // console.log("Get married")
-}
+    if (age>18){
+        alert("You can legally drink and smoke");
+        // console.log("But should you?")
+    }
 
-if (age>45){
-    alert("Best age for retirement")
-    // console.log("Get a second wife")
-}
+    if (age>27){
+        alert("Best age for marriage")
+        // console.log("Get married")
+    }
 
-if (age>50){
-    // console.log("You are expired")
-    alert("You are an ancestor")
-}
+    if (age>45){
+        alert("Best age for retirement")
+        // console.log("Get a second wife")
+    }
+
+    if (age>50){
+        // console.log("You are expired")
+        alert("You are an ancestor")
+    }
+// } else {
+//     alert("Please enter a number")
+// }
