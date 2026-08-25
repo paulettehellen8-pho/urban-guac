e7321eea4e147be300dcf71c2f1b5d7e48802050
paulettehellen8-pho <paulettehellen8@gmail.com>
2026-08-25 10:15:37 +0300

    intro to python

---
+++ PYTHON/Intro/data_type.py
@@ -0,0 +1,19 @@
+# Variables
+x=10 #int 
+y=3.14 #float
+z="Hello" #str
+a=True #boolean
+b=[1,2,3,3,4] #list
+c={1,2,3} #set -> only unique values
+d=(1,2,3) # Tuple -> is immutable=> cannot be changed
+e={"key":"value"} #dict <object.js>
+
+
+print("X is ",x, "Its type is ", type(x))
+print(f"Y is {y} Its type is  {type(y)}") #`y is ${}` js
+print("Z is ",z, "Its type is ", type(z))
+print("A is ",a, "Its type is ", type(a))
+print("B is ",b, "Its type is ", type(b))
+print("C is ",c, "Its type is ", type(c))
+print("D is ",d, "Its type is ", type(d))
+print(f"E is {e} Its type is  {type(e)}")
---
+++ PYTHON/Intro/start.py
@@ -0,0 +1,9 @@
+# comments
+# console.log -> print
+
+x=34
+print("X is = ", x)
+y=20
+print("Y is = ", y)
+z=x+y
+print("Z is = ", z)
