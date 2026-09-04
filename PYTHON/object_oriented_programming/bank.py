99f860abe4fb5380d60d17a7572944e1cf5086bc
paulettehellen8-pho <paulettehellen8@gmail.com>
2026-09-04 10:19:47 +0300

    static and class methods

--- PYTHON/object_oriented_programming/bank.py
+++ PYTHON/object_oriented_programming/bank.py
@@ -14,11 +14,26 @@ Classes are easy to scale functionality with proper understanding
 -> Acc balance
 """
 
+
+"""
+Static <it does not change> properties <Belong to a class>
+Static method <Class Method> Function belongs to the class
+
+Why would you want to use them
+"""
+
 class BankAccount:
+   clients = 0
+   bank_name="Post Bank"
+
    def __init__(self, name, balance, acc_no):
       self.name=name
       self._balance=balance
       self.acc_no=acc_no
+      #BankAccount.clients += 1
+      #self.__class__.clients +=1 
+
+      BankAccount.add_client()#call the class method
 
    #later change it to a getter
    #control how data is read
@@ -42,7 +57,7 @@ class BankAccount:
          self._balance=value
 
    #later change it to a setter
-   def deposit(self):   
+   def deposit(self):   #instance method
       pass
 
    def withdrawal(self):   
@@ -53,6 +68,34 @@ class BankAccount:
          print(f"Balance {self.balance}")
          print(f"Account No. {self.acc_no}")
 
+   #---------------------------------------------
+
+   #Static method. @staticmethod 
+   #    
+   #---------------------------------------------
+   @staticmethod
+   def calculate_loan_interest(amount,year):
+       rate=10
+       interest_per_year=amount*(rate/100)
+       interest_total=interest_per_year*year
+       total=amount+interest_total
+       print("-------------------------------------------------------------------------------------")
+       print(f"If you take a loan of ${amount}, interest rate per year is ${interest_per_year}")
+       print(f"Total interest {interest_total}. Total to pay {total} after {year} year(s)")
+
+
+   #---------------------------------------------
+
+   #Class method. @classmethod
+
+   #---------------------------------------------
+   @classmethod
+   def add_client(cls):
+       cls.clients += 1
+
+
+
+
 phobi=BankAccount(name="Phobi", balance=10000000, acc_no="223344223") 
 
 print("Phobi balance", phobi.balance)
@@ -60,4 +103,11 @@ phobi.show_account_details()
 phobi.balance=-30000 #setting -> updating property
 phobi.show_account_details()
 
-#Getters and setters
+print(BankAccount.bank_name)#class property
+print(phobi.acc_no)# instance attr
+print("Total Clients", BankAccount.clients)
+dan=BankAccount(name="Phobi", balance=10000000, acc_no="223344223") 
+print("Total Clients", BankAccount.clients)
+
+print("---------------------------------------------")
+BankAccount.calculate_loan_interest(50000,3)
